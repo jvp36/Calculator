836e2e9dda7445d870b153e769cd5a225b10fb86
jvp36 <jvp.36@yahoo.com>
2024-12-12 09:50:56 -0400

    Add basic math operations

---
+++ script.js
@@ -0,0 +1,21 @@
+// functions
+// basic operations
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+// operate function
+const operate = (operator, num1, num2) => {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2); 
+    } else {
+        return divide(num1, num2);
+    }
+}
+
+
+
